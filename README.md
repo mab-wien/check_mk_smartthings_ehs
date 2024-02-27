fa8b98d246f052e56701586fd4d188b5fbc86434
devops-wien <devops.wien@gmail.com>
2024-02-27 15:52:51 +0100

    update README.md, add smartthings_ehs.sh - initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # check_mk_smartthings_ehs
-simple check_mk smartthings ehs temperatureMeasurementCheck (e.g. for Samsung Electronics - MIM-H04EN/DA-SAC-WIFIKIT-000001 with Subsystem: Eco Heating System (EHS) / SAC_EHS_MONO)
+
+Simple (DEMO) check_mk smartthings ehs temperatureMeasurementCheck
+
+## e.g. for Samsung Electronics
+
+- MIM-H04EN/DA-SAC-WIFIKIT-000001 with Subsystem Eco Heating System (EHS) / SAC_EHS_MONO (Quiet WP)
+
+## check req.
+
+- bash
+- curl
+- jq
+
+## Script directory
+
+See https://docs.checkmk.com/latest/en/localchecks.html#folders
+
+## Test
+
+```
+chmod +x smartthings_ehs.sh
+./smartthings_ehs.sh $bearerToken $deviceId
+```
+
+## SmartThings
+
+### getBearerToken
+
+See https://account.smartthings.com/tokens
+
+### getDeviceId
+
+See https://developer.smartthings.com/docs/api/public/#tag/Devices/operation/getDevices
---
+++ smartthings_ehs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+platformUrl="https://api.smartthings.com/v1"
+bearerToken="$1"
+deviceId="$2"
+name="smartthings_ehs"
+configFile=/etc/smartthings.conf
+labels=(power powerEnergy energy deltaEnergy waterTemp waterTempSet heatingTemp HeatingTempSet)
+
+fail() {
+  echo 2 $name "-" "$2"
+  exit "$1"
+}
+getDeviceStatus() {
+  curl -s --location "$platformUrl/devices/$deviceId/status" \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $bearerToken" \
+    --header 'Accept: application/vnd.smartthings+json;v=1' |
+    jq " \
+ .components.main.powerConsumptionReport.powerConsumption.value.power, \
+ .components.main.powerConsumptionReport.powerConsumption.value.powerEnergy, \
+ .components.main.powerConsumptionReport.powerConsumption.value.energy, \
+ .components.main.powerConsumptionReport.powerConsumption.value.deltaEnergy, \
+ .components.main.temperatureMeasurement.temperature.value, \
+ .components.main.thermostatCoolingSetpoint.coolingSetpoint.value, \
+ .components.INDOOR.temperatureMeasurement.temperature.value, \
+ .components.INDOOR.thermostatCoolingSetpoint.coolingSetpoint.value \
+ "
+}
+
+if [ "$deviceId" == "" ]; then
+  if test -f "$configFile"; then
+    # shellcheck disable=SC1090
+    . $configFile
+  else
+    fail 2 "USAGE: $0 \$bearerToken \$deviceId or use configFile $configFile"
+  fi
+fi
+
+ret="$(getDeviceStatus)"
+exitCode="$?"
+
+if [ "$exitCode" != "0" ]; then
+  fail $exitCode
+fi
+
+retValue=""
+cnt=0
+while IFS= read -r value; do
+  if [ "$retValue" == "" ]; then
+    retValue="${labels[$cnt]}=$value"
+  else
+    retValue="$retValue|${labels[$cnt]}=$value"
+  fi
+  cnt=$((cnt + 1))
+done <<<"$ret"
+
+echo $exitCode $name "$retValue" A service with $cnt graphs
